0b6d89cd0e686e52c5b4fc7f1a7337b5d29b49c2
Nikita Naumov <neknaum1@gmail.com>
2024-08-26 17:52:40 +0300

    Add dataloader

--- src/model.py
+++ src/model.py
@@ -2,12 +2,13 @@ from ncps.torch import LTC
 from ncps.wirings import AutoNCP
 from src.encoder import Encoder, EncoderResnet18
 
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import torchvision.models as models
-from torchvision.io import read_image
+import torch.utils as utils
+
 from torchvision.transforms.functional import pil_to_tensor
 from torch.utils.data import Dataset
 from torch.utils.tensorboard import SummaryWriter
@@ -76,16 +77,22 @@ class TrainingDataset(Dataset):
         self.image_and_steer = pd.read_csv(annotations_file, sep=":", names=['Image', 'Steer_angle'])
         self.image_and_steer = self.image_and_steer.groupby(by=['Image']).Steer_angle.mean().reset_index()
         self.image_and_steer = self.image_and_steer.to_dict('list')
-        image_names = self.image_and_steer['Image']
-        for idx, img_name in enumerate(image_names):
+        self.image_names = self.image_and_steer['Image']
+        for idx, img_name in enumerate(self.image_names):
             image = np.array(Image.open(f'{img_dir}/{img_name}.png'))
             self.image_and_steer['Image'][idx] = torch.from_numpy(image[:, :, :3]).permute(2,0,1)
+
+    def __len__(self):
+        return len(self.image_and_steer)
+    
+    def __getitem__(self, index):
+        return self.image_and_steer[self.image_names[index]]
 
     def train_test_split(self, test_size=0.2, random_state=42):
         to_split = []
         for item in pd.DataFrame.from_records(self.image_and_steer).iterrows():
             to_split.append((item[1]['Image'], item[1]['Steer_angle']))
-        print(to_split)
+        # print(to_split)
         self.train, self.test = train_test_split(to_split,
                                                  test_size=test_size,
                                                  random_state=random_state)
@@ -114,11 +121,13 @@ class Trainer:
         running_loss = 0
         last_loss = 0
         train_ds, test_ds = self.Dataset.train_test_split(test_size=self.test_size, random_state=self.random_state)
-        train_ds = train_ds.to(self.device)
-        test_ds = test_ds.to(self.device)
+        train_dl = utils.data.DataLoader(train_ds,
+                                         batch_size=64,
+                                         shuffle=True,
+                                         num_workers=6)
 
-        for i, data in enumerate(train_ds):
-            image, true_angle = data
+        idx = 0
+        for image, true_angle in tqdm(train_dl):
             # image = pil_to_tensor(image)
             # image = torch.from_numpy(image)
             # print(f'Img={image}, angl = {true_angle}')
@@ -128,12 +137,13 @@ class Trainer:
             loss.backward()
             self.optimizer.step()
             running_loss += loss.item()
-            if i % 100 == 0:
+            if idx % 100 == 0:
                 last_loss = running_loss / 100 # loss per batch
-                print(f'  batch {i+1} loss: {last_loss}')
-                tb_x = epoch * len(train_ds) + i + 1
+                print(f'  batch {idx} loss: {last_loss}')
+                tb_x = epoch * len(train_ds) + idx + 1
                 logger.add_scalar('Loss/train', last_loss, tb_x)
-                running_loss = 0.
+                running_loss = 0.0
+            idx += 1
         return last_loss
 
     def train(self, epochs=10):
